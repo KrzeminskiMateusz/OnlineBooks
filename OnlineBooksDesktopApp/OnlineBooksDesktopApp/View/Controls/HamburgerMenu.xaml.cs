6bfdb82d31498b95e4613729abae1f8780ae6c35
Mateusz Krzemiński <Mateusz Krzemiski@PCMKRZEMINSKI>
2020-10-22 15:42:29 +0200

    .

--- OnlineBooksDesktopApp/OnlineBooksDesktopApp/View/Controls/HamburgerMenu.xaml.cs
+++ OnlineBooksDesktopApp/OnlineBooksDesktopApp/View/Controls/HamburgerMenu.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -23,11 +24,27 @@ namespace OnlineBooksDesktopApp.View.Controls
             InitializeComponent();
         }
 
-        private void UserControl_SizeChanged(object sender, SizeChangedEventArgs e)
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
         {
-            HomeButton.Height = this.ActualHeight * 0.2;
-            HomeImage.Height = HomeButton.Height;
-            HomeImage.Width = HomeButton.Height;
+            SetLanguageDictionary();
+        }
+
+        private void SetLanguageDictionary()
+        {
+            ResourceDictionary dict = new ResourceDictionary();
+            switch (Thread.CurrentThread.CurrentCulture.ToString())
+            {
+                case "en-US":
+                    dict.Source = new Uri(@"\Resources\Languages\DictionaryENG.xaml", UriKind.Relative);
+                    break;
+                case "pl-PL":
+                    dict.Source = new Uri(@"\Resources\Languages\DictionaryPL.xaml", UriKind.Relative);
+                    break;
+                default:
+                    dict.Source = new Uri(@"\Resources\Languages\DictionaryPL.xaml", UriKind.Relative);
+                    break;
+            }
+            this.Resources.MergedDictionaries.Add(dict);
         }
     }
 }
